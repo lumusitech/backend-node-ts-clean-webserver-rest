9c70497e2e55f5ee85698421c32574a9a25451e6
Luciano Figueroa <lumusitech@gmail.com>
2024-02-22 20:54:45 -0300

    feat: add basic express server

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,10 @@
-import fs from 'fs'
-import http2 from 'http2'
+import { Server } from './presentation/server'
+;(async () => {
+  main()
+})()
 
-const server = http2.createSecureServer(
-  {
-    key: fs.readFileSync('./keys/server.key'),
-    cert: fs.readFileSync('./keys/server.crt'),
-  },
-  (req, res) => {
-    // Add a logger like morgan here
-    console.log(req.url)
+function main() {
+  const server = new Server()
 
-    switch (req.url) {
-      case '/':
-        const htmlBody = fs.readFileSync('./public/index.html', 'utf-8')
-
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(htmlBody)
-        break
-
-      case '/css/styles.css':
-        const cssBody = fs.readFileSync('./public/css/styles.css', 'utf-8')
-        res.writeHead(200, { 'Content-Type': 'text/css' })
-        res.end(cssBody)
-        break
-
-      case '/js/app.js':
-        const jsBody = fs.readFileSync('./public/js/app.js', 'utf-8')
-        res.writeHead(200, { 'Content-Type': 'application/javascript' })
-        res.end(jsBody)
-        break
-
-      case '/favicon.ico':
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        res.end()
-        break
-
-      case '/api':
-        const data = {
-          name: 'John Doe',
-          age: 30,
-          city: 'New York',
-        }
-
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(data))
-        break
-
-      default:
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        res.end('<h1>404 Not Found</h1>')
-        break
-    }
-  },
-)
-
-const PORT = 8000
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  server.start()
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+export class Server {
+  private app = express()
+
+  public start() {
+    this.app.use(express.static('public'))
+
+    this.app.listen(3000, () => {
+      console.log('Server running on port 3000')
+    })
+  }
+}
