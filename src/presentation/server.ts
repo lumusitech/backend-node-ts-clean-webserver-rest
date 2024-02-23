74531bd77cdd5933272dec47dfff638a0044729e
Luciano Figueroa <lumusitech@gmail.com>
2024-02-23 12:05:49 -0300

    feat: add and config react app with its routes

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,22 @@
 import express from 'express'
+import path from 'path'
 
 export class Server {
   private app = express()
 
   public start() {
+    // Middlewares
+
+    this.app.use(express.json())
+
+    // public folder
     this.app.use(express.static('public'))
+
+    this.app.get('*', (req, res) => {
+      const indexPath = path.join(__dirname, '../../public/index.html')
+
+      res.sendFile(indexPath)
+    })
 
     this.app.listen(3000, () => {
       console.log('Server running on port 3000')
