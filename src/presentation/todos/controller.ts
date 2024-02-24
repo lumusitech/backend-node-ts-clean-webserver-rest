fec90bda6f998b3109e11368d1c6d8e91bb1ce77
Luciano Figueroa <lumusitech@gmail.com>
2024-02-23 23:57:34 -0300

    feat: add delete todo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -63,4 +63,21 @@ export class TodosController {
 
     return res.json(todo)
   }
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const id = +req.params.id
+
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'ID argument is not a number' })
+    }
+
+    const todo = todos.find(todo => todo.id === id)
+
+    if (!todo) {
+      return res.status(404).json({ error: `Todo with id ${id} not found` })
+    }
+
+    todos.splice(todos.indexOf(todo), 1)
+    return res.json(todo)
+  }
 }
