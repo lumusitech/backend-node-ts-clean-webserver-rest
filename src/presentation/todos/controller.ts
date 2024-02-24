24a4f80558c13022fa04d00fdb9682c8af129144
Luciano Figueroa <lumusitech@gmail.com>
2024-02-23 22:40:42 -0300

    feat: add update todo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
 
 const todos = [
-  { id: 1, text: 'some task todo 1', createdAt: new Date() },
-  { id: 2, text: 'some task todo 2', createdAt: null },
-  { id: 3, text: 'some task todo 3', createdAt: new Date() },
+  { id: 1, text: 'some task todo 1', completedAt: new Date() },
+  { id: 2, text: 'some task todo 2', completedAt: null },
+  { id: 3, text: 'some task todo 3', completedAt: new Date() },
 ]
 
 export class TodosController {
@@ -34,10 +34,33 @@ export class TodosController {
     const todo = {
       id: todos.length + 1,
       text,
-      createdAt: new Date(),
+      completedAt: null,
     }
 
     todos.push(todo)
     return res.status(201).json(todo)
+  }
+
+  public updateTodo = (req: Request, res: Response) => {
+    const id = +req.params.id
+    const { text, completedAt } = req.body
+
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'ID argument is not a number' })
+    }
+
+    const todo = todos.find(todo => todo.id === id)
+
+    if (!todo) {
+      return res.status(404).json({ error: `Todo with id ${id} not found` })
+    }
+
+    todo.text = text || todo.text
+
+    completedAt === 'null'
+      ? (todo.completedAt = null)
+      : (todo.completedAt = new Date(completedAt || todo.completedAt))
+
+    return res.json(todo)
   }
 }
