0d520df312bb1a1e43a6c1c6d2f917c4f0516009
Luciano Figueroa <lumusitech@gmail.com>
2024-02-23 21:23:45 -0300

    feat: add get todo by id

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,17 +1,26 @@
 import { Request, Response } from 'express'
 
+const todos = [
+  { id: 1, text: 'some task todo 1', createdAt: new Date() },
+  { id: 2, text: 'some task todo 2', createdAt: null },
+  { id: 3, text: 'some task todo 3', createdAt: new Date() },
+]
+
 export class TodosController {
   // DI
   constructor() {}
 
-  public getTodos = (req: Request, res: Response) => {
-    return res.json({
-      ok: true,
-      todos: [
-        { id: 1, text: 'some task todo 1', createdAt: new Date() },
-        { id: 2, text: 'some task todo 2', createdAt: null },
-        { id: 3, text: 'some task todo 3', createdAt: new Date() },
-      ],
-    })
+  public getTodos = (req: Request, res: Response) => res.json(todos)
+
+  public getTodosById = (req: Request, res: Response) => {
+    const id = +req.params.id // '+' convert to number
+
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'ID argument is not a number' })
+    }
+
+    const todo = todos.find(todo => todo.id === id)
+
+    return todo ? res.json(todo) : res.status(404).json({ error: `Todo with id ${id} not found` })
   }
 }
