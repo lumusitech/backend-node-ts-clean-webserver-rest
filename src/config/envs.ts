d4281ab6535ab6ef41788f98ce0b9fc9cdf7951d
Luciano Figueroa <lumusitech@gmail.com>
2024-02-24 22:47:27 -0300

    chore: add new env variables to config folder

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,4 +4,10 @@ import { get } from 'env-var'
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
+  POSTGRES_URL: get('POSTGRES_URL').asString(),
+  POSTGRES_USER: get('POSTGRES_USER').asString(),
+  POSTGRES_DB: get('POSTGRES_DB').asString(),
+  POSTGRES_PORT: get('POSTGRES_PORT').asString(),
+  POSTGRES_PASSWORD: get('POSTGRES_PASSWORD').asString(),
+  NODE_ENV: get('NODE_ENV').asString(),
 }
