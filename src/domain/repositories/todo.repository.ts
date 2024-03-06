5ee913b01a79dc19ce1505fde3e42d46528e2176
Luciano Figueroa <lumusitech@gmail.com>
2024-03-05 21:39:31 -0300

    feat: add todo repository into domain

---
+++ src/domain/repositories/todo.repository.ts
@@ -0,0 +1,10 @@
+import type { CreateTodoDto, UpdateTodoDto } from '../dtos'
+import type { TodoEntity } from '../entities/todo.entity'
+
+export abstract class TodoRepository {
+  abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>
+  abstract deleteById(id: number): Promise<void>
+  abstract findById(id: number): Promise<TodoEntity>
+  abstract getAll(): Promise<TodoEntity[]> // todo: add pagination and others by props
+  abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>
+}
