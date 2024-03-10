8f0f5a24db935adcb4456d0e068f49f1c5d02adb
luciano <lumusitech@gmail.com>
2024-03-10 17:40:12 -0300

    test: add app.ts testing

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,19 @@
-describe("App", () => {
-  it("should run", () => {
-    expect(1).toBe(1);
-  });
-});
+import { envs } from '../src/config/envs'
+import { Server } from '../src/presentation/server'
+
+jest.mock('../src/presentation/server')
+
+describe('App', () => {
+  it('should call the server with options', async () => {
+    await import('../src/app')
+
+    expect(Server).toHaveBeenCalledTimes(1)
+    expect(Server).toHaveBeenCalledWith({
+      port: 3001,
+      publicPath: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    })
+
+    expect(Server.prototype.start).toHaveBeenCalled()
+  })
+})
