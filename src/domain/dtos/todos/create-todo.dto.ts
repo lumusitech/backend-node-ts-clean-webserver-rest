90b4c88d4fdb65b08b719341dd146352035c38fe
luciano <lumusitech@gmail.com>
2024-03-11 18:29:18 -0300

    feat: update create dto and controller for testing

--- src/domain/dtos/todos/create-todo.dto.ts
+++ src/domain/dtos/todos/create-todo.dto.ts
@@ -4,7 +4,7 @@ export class CreateTodoDto {
   static create(props: { [key: string]: any }): [string?, CreateTodoDto?] {
     const { text } = props
 
-    if (!text) return ['text is required', undefined]
+    if (!text || text.length === 0) return ['text is required', undefined]
 
     return [undefined, new CreateTodoDto(text)]
   }
